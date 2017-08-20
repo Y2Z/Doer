a59c479e03a288ae157b9a79c4321ebc7df69654
Vincent Flyson <vflyson@uberspace.net>
2017-08-19 23:03:27 -0400

    Allow to run doer as a different user on the same machine

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 #include "mainwindow.h"
 #include "runguard.h"
 
+QString getUserName()
+{
+    QString name = qgetenv("USER");
+
+    if (name.isEmpty())
+        name = qgetenv("USERNAME");
+
+    return name;
+}
 
 int main(int argc, char **argv)
 {
-    RunGuard guard("UwNDcyZjc6MjE1tN2Ma3NGIf5OGx");
+    RunGuard guard(getUserName() + "-doer-UwNDcyZjc6MjE1tN2Ma3NGIf5OGx");
 
     if (!guard.tryToRun())
         return 0;
